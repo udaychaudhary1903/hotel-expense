c395d12100f40a9226df56880a872b056275b757
Uday Chaudhary <your-email@example.com>
2026-07-05 19:14:55 +0530

    hide api keys using env variables

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBdI6WfOq0AHgodek1lrD6f71DbLDUErQ",
-  authDomain: "restaurant-calc-4daa2.firebaseapp.com",
-  projectId: "restaurant-calc-4daa2",
-  storageBucket: "restaurant-calc-4daa2.firebasestorage.app",
-  messagingSenderId: "650245627215",
-  appId: "1:650245627215:web:69a9b35a822723d1b5393a",
-  measurementId: "G-BG8PR431V5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
